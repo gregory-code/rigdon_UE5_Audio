508b2b2190851bb15dfdfe152751a24051b49063
gregory-code <111695375+gregory-code@users.noreply.github.com>
2024-04-14 16:39:19 -0500

    Set up
    made some new scripts

---
+++ Source/audioDemo/Private/Character/PlayerCharacter.cpp
@@ -0,0 +1,95 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/PlayerCharacter.h"
+#include "GameFramework/SpringArmComponent.h" // have to include headers
+#include "Camera/CameraComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Engine/World.h"
+#include "Kismet/GameplayStatics.h"
+#include "Blueprint/UserWidget.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+// Sets default values
+APlayerCharacter::APlayerCharacter()
+{
+	cameraBoom = CreateDefaultSubobject<USpringArmComponent>("Camera Boom");
+	viewCamera = CreateDefaultSubobject<UCameraComponent>("View Camera");
+	//inventoryComp = CreateDefaultSubobject<UInventoryComponent>("Inventory Component"); need the inventory component
+
+	cameraBoom->SetupAttachment(GetRootComponent());
+	viewCamera->SetupAttachment(cameraBoom, USpringArmComponent::SocketName);
+
+	cameraBoom->bUsePawnControlRotation = true;
+	cameraBoom->TargetArmLength = 500.0f;
+
+	bUseControllerRotationYaw = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(1080.0f);
+
+	GetCharacterMovement()->JumpZVelocity = 600.0f;
+}
+
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FInputModeGameOnly input;
+	GetWorld()->GetFirstPlayerController()->SetInputMode(input);
+}
+
+void APlayerCharacter::PawnClientRestart() // Treating this as begin play ???
+{
+	Super::PawnClientRestart();
+
+	APlayerController* PlayerController = GetController<APlayerController>();
+	if (PlayerController)
+	{
+		UEnhancedInputLocalPlayerSubsystem* InputSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+		InputSystem->ClearAllMappings();
+		InputSystem->AddMappingContext(inputMapping, 0);
+	}
+}
+
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	UEnhancedInputComponent* enhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	if (enhancedInputComponent)
+	{
+		enhancedInputComponent->BindAction(moveInputAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Move);
+		enhancedInputComponent->BindAction(lookInputAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Look);
+	}
+}
+
+void APlayerCharacter::Move(const FInputActionValue& InputValue)
+{
+	FVector2D input = InputValue.Get<FVector2D>();
+	input.Normalize();
+
+	AddMovementInput(input.Y * GetMoveFwdDir() + input.X * GetMoveRightDir());
+}
+
+void APlayerCharacter::Look(const FInputActionValue& InputValue)
+{
+	FVector2D input = InputValue.Get<FVector2D>();
+	AddControllerYawInput(input.X);
+	AddControllerPitchInput(-input.Y);
+}
+
+
+FVector APlayerCharacter::GetMoveFwdDir() const
+{
+	FVector CameraFwd = viewCamera->GetForwardVector();
+	CameraFwd.Z = 0;
+
+	return CameraFwd.GetSafeNormal();
+}
+
+FVector APlayerCharacter::GetMoveRightDir() const
+{
+	return viewCamera->GetRightVector();
+}
---
+++ Source/audioDemo/Private/Character/PlayerCharacter.h
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Framework/AudioPlayerController.h"
+#include "PlayerCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+class AAudioPlayerController;
+
+UCLASS()
+class APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	APlayerCharacter();
+
+	AAudioPlayerController* MyPlayerController;
+
+private:
+	UPROPERTY(visibleAnywhere, Category = "View")
+	USpringArmComponent* cameraBoom;
+
+	UPROPERTY(visibleAnywhere, Category = "View")
+	UCameraComponent* viewCamera;
+
+	//UPROPERTY(visibleAnywhere, Category = "Inventory")
+	//UInventoryComponent* inventoryComp;
+
+	virtual void PawnClientRestart() override;
+
+	/********************************************/
+	/*                Input                     */
+	/********************************************/
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputMappingContext* inputMapping;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputAction* moveInputAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputAction* lookInputAction;
+
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION()
+	void Move(const FInputActionValue& InputValue);
+
+	UFUNCTION()
+	void Look(const FInputActionValue& InputValue);
+
+	FVector GetMoveFwdDir() const;
+	FVector GetMoveRightDir() const;
+
+};
